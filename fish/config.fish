c893a8c601c7e212a40ae580c85091151855fa2e
Dylan Mooney <dylan@dylanmooney.co.uk>
2022-09-03 16:17:08 +0100

    Refactor fish config and add prompt settings

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,24 @@
 if status is-interactive
 
-    # Disable the fish greeting
-    set fish_greeting
+    # Aliases
+    alias ls="pls"
 
     # Disable Gatekeeper
     set -gx HOMEBREW_CASK_OPTS --no-quarantine
 
+    # Disable the fish greeting
+    set fish_greeting
+
+    # Customise prompt
+    set -U tide_left_prompt_items pwd 
+    set -U tide_pwd_color_dirs EA5F72
+    set -U tide_pwd_color_anchors DDDDDD
+    
+    set -U tide_right_prompt_items git
+    set -U tide_git_color_branch FAD649
+
     # Setup VSCode code command
-    function code
+    function code --description 'Open VSCode'
         set location "$PWD/$argv"
         open -n -b "com.microsoft.VSCode" --args $location
     end
