e4be20fe5e8be345161ae3f1b63e2ca8ffafc53e
Dylan Mooney <dylan@dylanmooney.co.uk>
2022-09-03 16:40:24 +0100

    Add plugins

--- fish/install.fish
+++ fish/install.fish
@@ -3,3 +3,7 @@
 echo Installing Fish packages... 🐟
 
 fisher install IlanCosman/tide@v5
+fisher install jethrokuan/z
+fisher install jorgebucaran/nvm.fish
+fisher install franciscolourenco/done
+fisher install meaningful-ooo/sponge
